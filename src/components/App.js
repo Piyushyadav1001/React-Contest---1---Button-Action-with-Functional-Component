a79131b8b432b73117cce42db48e677ba261a46d
Piyushyadav1001 <72632167+Piyushyadav1001@users.noreply.github.com>
2020-11-06 21:50:42 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
-
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+import React, { Component} from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: false
+    };
+    this.btnClick = this.btnClick.bind(this);
+  }
+  btnClick() {
+    this.setState({
+      clicks: true
+    });
+  }
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.btnClick}>click
+        </button>
+        {this.state.clicks ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makesme so happy
+          </p>
+        ) : null}
+      </div>
+    );
+  }
 }
-
 
 export default App;
